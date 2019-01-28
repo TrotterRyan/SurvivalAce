526d32306a04674c3408c4042eaa5972f45f4425
DatWest <47054086+DatWest@users.noreply.github.com>
2019-01-28 16:53:43 +0000

    Shoot

---
+++ SurvivalAceGame/Assets/Script/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    
+    public float BulletSpeed = 5;
+
+    void Update()
+    {
+        transform.Translate(new Vector3(0, BulletSpeed * Time.deltaTime, 0));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        Destroy(gameObject);
+    }
+
+}
